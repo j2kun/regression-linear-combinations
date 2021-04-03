d0117797260d4a4f0be55bd16889fae8166e9b4d
Jeremy Kun <j2kun@users.noreply.github.com>
2021-04-03 07:44:19 -0700

    fix mypy type errors

--- quadratic_regression.py
+++ quadratic_regression.py
@@ -3,8 +3,8 @@ import random
 
 
 Input = Tuple[float, float, float]
-Coefficients = Tuple[float, float, float]
-Gradient = Tuple[float, float, float]
+Coefficients = List[float]
+Gradient = List[float]
 Hypothesis = Callable[[Input], float]
 Dataset = List[Tuple[Input, float]]
 
@@ -60,7 +60,7 @@ def gradient(weights: Coefficients, data_point: Tuple[Input, float]) -> Gradient
     '''Return the gradient of the error with respect to each weight.'''
     error = single_point_error(weights, data_point)
     return [
-            -2 * error * basis[i](data_point[0])
+        -2 * error * basis[i](data_point[0])
         for i in range(len(weights))
     ]
 
@@ -73,7 +73,7 @@ def gradient_descent(
         data: Dataset,
         learning_rate: float,
         tolerance: float,
-        training_callback = None,
+        training_callback=None,
 ) -> Hypothesis:
     weights = [
         random.random() * 2 - 1
@@ -83,14 +83,14 @@ def gradient_descent(
     last_error = total_error(weights, data)
     step = 0
     progress = tolerance * 2
-    grad_norm = 1
+    grad_norm = 1.0
 
     if training_callback:
         training_callback(step, 0.0, last_error, 0.0)
 
     while abs(progress) > tolerance or grad_norm > tolerance:
         grad = gradient(weights, random.choice(data))
-        grad_norm = sum(x**2 for x in grad)
+        grad_norm = sum(x ** 2 for x in grad)
         for i in range(len(weights)):
             weights[i] -= learning_rate * grad[i]
 
@@ -120,8 +120,8 @@ def example_quadratic_data(num_points: int):
 if __name__ == "__main__":
     data = example_quadratic_data(30)
     gradient_descent(
-        data, 
-        learning_rate=0.01, 
-        tolerance=1e-06, 
+        data,
+        learning_rate=0.01,
+        tolerance=1e-06,
         training_callback=print_debug_info
     )
